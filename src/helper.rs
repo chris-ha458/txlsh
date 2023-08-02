15dea727205eb528f180bf5d96827e67fc2f6f41
Chris Ha <hac541309@gmail.com>
2023-08-03 06:09:21 +0900

    Update helper.rs
    more tests

--- src/helper.rs
+++ src/helper.rs
@@ -275,3 +275,16 @@ unsafe fn bit_pairs_diff(row: usize, col: usize) -> usize {
 
     BIT_PAIRS_DIFF[row][col]
 }
+
+
+#[cfg(test)]
+mod tests {
+    use crate::TxLsh;
+
+    use super::*;
+
+    static LOREM_0: &[u8] = b"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    fn diff_test(hash:TxLsh) -> bool {
+        true
+    }
+}
